be290f674d3fd36391ec1fd325a6a92c07542bee
godyin99 <zhiq05@163.com>
2020-05-15 17:57:30 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
+
 function setUserName() {
   let myName = prompt('请输入您的名字');
+  if(!myName||myName === null) {
+  setUserName();
+  } else {
   localStorage.setItem('name', myName);
   myHeading.textContent = 'Mozilla 酷毙了，'+ myName;
-
+  }
 };
 
 if(!localStorage.getItem('name')) {
