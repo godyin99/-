5dd6944d9fb65218f5bab9cf33df3889ad8af915
godyin99 <zhiq05@163.com>
2020-05-15 17:29:53 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,15 @@
+let myButton = document.querySelectou('button');
 let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+function setUserName() {
+  let myName = prompt('请输入您的名字');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla 酷毙了，'+ myName;
+
+};
+
+if(!localStorage.setItem('name')) {
+  setUserName();
+  } else {
+  let storedName = localStorage.setItem('name');
+  myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+  }
